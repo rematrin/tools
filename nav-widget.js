77aa76e5aa4ed2e3ac2cf3f8e2843cdb4afe1bf6
MacBookPro <rematrin@gmail.com>
2026-05-08 13:01:44 +0200

    feat: add bookmarks page and link it in navigation widget

--- nav-widget.js
+++ nav-widget.js
@@ -69,6 +69,10 @@ const servicesLinks = [
     {
         href: "eleven_reader.html", title: "Eleven Reader",
         icon: '<path d="M12 1a3 3 0 0 0-3 3v8a3 3 0 0 0 6 0V4a3 3 0 0 0-3-3z"></path><path d="M19 10v2a7 7 0 0 1-14 0v-2"></path><line x1="12" y1="19" x2="12" y2="23"></line><line x1="8" y1="23" x2="16" y2="23"></line>'
+    },
+    {
+        href: "bookmarks.html", title: "Мои закладки",
+        icon: '<path d="M19 21l-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2z"></path>'
     }
 
 ];

43aa6b76efaa69c8fd67867e8274679b5a897e66
MacBookPro <rematrin@gmail.com>
2026-07-21 17:47:48 +0200

    feat: persist merged track data to currentSavedPlaylists and optimize cachedMap lookup logic

--- auth-widget.js
+++ auth-widget.js
@@ -16,6 +16,9 @@ import {
     doc,
     setDoc,
     getDoc,
+    deleteDoc,
+    collection,
+    getDocs,
     serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
